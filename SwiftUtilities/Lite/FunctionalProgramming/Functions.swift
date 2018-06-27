04508d9711b8e13dc1393735e99354bb56ecc19d
Rodrigo Ruiz <rodrigo.ruiz7@gmail.com>
2018-06-27 15:24:10 -0300

    Add helper functions.

--- SwiftUtilities/Lite/FunctionalProgramming/Functions.swift
+++ SwiftUtilities/Lite/FunctionalProgramming/Functions.swift
@@ -15,3 +15,27 @@ public func asType<T, V>(_ type: T.Type) -> (V) -> T? {
         return value as? T
     }
 }
+
+func const<C, I>(_ c: C) -> (I) -> C {
+    return { _ in c }
+}
+
+func ifElse<I, O>(
+    _ condition: @escaping (I) -> Bool,
+    _ ifTransform: @escaping (I) -> O,
+    _ elseTransform: @escaping (I) -> O
+) -> (I) -> (O) {
+    return { input in
+        if condition(input) {
+            return ifTransform(input)
+        } else {
+            return elseTransform(input)
+        }
+    }
+}
+
+func getKeyPath<E, V>(_ keyPath: KeyPath<E, V>) -> (E) -> V {
+    return { element in
+        return element[keyPath: keyPath]
+    }
+}
